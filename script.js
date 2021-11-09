22caef445cd3171120271b10cd75ef46614bf754
FreezerFog <djwaccount+github@protonmail.com>
2021-11-08 20:06:29 -0800

    Initiate project files

---
+++ script.js
@@ -0,0 +1,7 @@
+// PSUEDOCODE
+// Prompt user for their selection of rock, paper, or scissors
+// Store the users selection
+// Computer randomly selects rock, paper, or scissors
+// Store the computers selection
+// Compare the computer against the player
+// Declare the winner based on their choice
